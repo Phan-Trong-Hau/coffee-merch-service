4c380b211af0c5c3e5568d9fed1870e82388cea7
PhanTrongHau <haupk2003@gmail.com>
2025-05-22 11:55:12 +0700

    fix: allow all

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-const connectDB = require("./src/config/database");
-const merchRoutes = require("./src/routes/merch.routes");
+const connectDB = require('./src/config/database');
+const merchRoutes = require('./src/routes/merch.routes');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5003;
@@ -12,21 +12,21 @@ const PORT = process.env.PORT || 5003;
 // Enable All CORS Requests
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   })
 );
 
 // Middleware
-app.use(express.json({limit: "50mb"}));
-app.use(express.urlencoded({ extended: true , limit: "50mb"}));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Kết nối MongoDB
 connectDB();
 
 // Routes
-app.use("/api/merch", merchRoutes);
+app.use('/api/merch', merchRoutes);
 
 // Run server
 app.listen(PORT, () => {
